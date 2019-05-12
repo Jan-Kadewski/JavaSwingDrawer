2c336a83ff7ef9f542c52014caf05554d99a5b85
Jan-Kadewski <jankadewski.it@gmail.com>
2019-05-12 18:07:38 +0200

    init project;Draw rectangle; todo:Draw oval

---
+++ src/Model/MyRectangle.java
@@ -0,0 +1,16 @@
+package Model;
+
+public class MyRectangle {
+
+    public int scale =1;
+    public int widthRect = 10;
+    public int heightRect =10;
+    public int xRect = 200;
+    public int yRect = 200;
+
+
+    public MyRectangle getRect(){
+        return this;
+
+    }
+}
